5fe83c8b3988a455d5f70f36d20d7d15b670f313
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-01-13 02:43:32 +0800

    update Tech component
    added hover

--- src/components/Tech.vue
+++ src/components/Tech.vue
@@ -1,10 +1,31 @@
 <script setup lang="ts">
+import { defineProps, ref, computed } from 'vue'
 
+const props = defineProps({
+  name: String,
+  iconName: String,
+});
+
+const isHovered = ref(false)
+
+const iconSrc = computed(() =>
+  isHovered.value ? `/public/hover-${props.iconName}` : `/public/${props.iconName}`
+);
 </script>
 
 <template>
-    <div class="flex flex-col justify-center px-16 py-2 gap-2 items-center border-[#575757] border rounded-md">
-        <img src="../../public/scala.png">
-        <p class="font-sans font-thin text-sm tracking-wide text-[#666161]">Scala</p>
-    </div>
+  <div
+    class="flex flex-col justify-center px-16 py-2 gap-2 items-center border-[#575757] border rounded-md hover:bg-[#262626]"
+    @mouseenter="isHovered = true"
+    @mouseleave="isHovered = false"
+  >
+    <img
+      :src="iconSrc"
+      alt="icon"
+      class="transition-transform duration-300 ease-out"
+      :class="{ '-translate-y-1 scale-100': isHovered }"
+
+    />
+    <p class="font-sans font-thin text-sm tracking-wide text-[#666161]">{{ name }}</p>
+  </div>
 </template>
