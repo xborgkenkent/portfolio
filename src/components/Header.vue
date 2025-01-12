cb02893eb440393fb292074726459bd3a968d01e
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-01-12 18:48:03 +0800

    create header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="flex items-end justify-between text-[#FAFAFA]">
+        <div>
+            <p class="font-sans font-bold text-2xl tracking-wider">KENT REGIEL BUNCAL</p>
+        </div>
+        <div class="flex space-x-4 ">
+            <p class="font-mono text-base font-thin tracking-wide">about me</p> 
+            <p class="font-mono text-base font-thin tracking-wide">projects</p>
+            <p class="font-mono text-base font-thin tracking-wide">contact</p>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
