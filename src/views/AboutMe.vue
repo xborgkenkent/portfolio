18ac5a07e7713b7c0981d7ca657fc3bfaa48e13a
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-01-12 18:48:31 +0800

    create AboutMe.vue

---
+++ src/views/AboutMe.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import Header from "../components/Header.vue";
+import Tech from "../components/Tech.vue";
+</script>
+
+<template>
+    <div class="flex flex-col text-[#FAFAFA]">
+        <img src="../../public/cover1.png" class="size-full transition-transform duration-300 ease-out hover:scale-105">
+        <div class="pt-2 flex">
+            <div class="relative">
+                <img src="../../public/profile.jpg" class="size-24 rounded-full border-4 border-[#575757]">
+                <!-- Spinner-like border effect -->
+                <div class="absolute inset-0 rounded-full border-4 border-transparent hover:border-t-[#4CAF50] hover:border-l-[#4CAF50] hover:border-r-[#4CAF50]  transition-all duration-1000 ease-in-out hover:rotate-[420deg]"></div>
+            </div>
+            <div class="flex flex-col pl-6 pt-2 gap-2">
+                <div class="flex justify-center items-center">
+                    <p class="font-sans font-bold text-2xl text-[#FAFAFA] tracking-wider">Hey, I'm Kent</p>
+                    <p class="text-2xl hover:animate-wave">👋</p>
+                </div>
+                <div class="flex items-center gap-2">
+                    <p class="text-xs">🟢</p>
+                    <p class="font-sans font-thin text-base text-[#666161] tracking-wide">Available for work</p>
+                </div>
+            </div>
+        </div>
+        <p class="font-sans font-thin text-base pt-8 tracking-wide">I’m an experienced full-stack developer with a specialization in back-end development, complemented  by extensive  expertise in system automation and seamless integrations.</p>
+        <div class="grid grid-cols-4 pt-8 gap-3">
+            <Tech/>
+            <Tech/>
+            <Tech/>
+            <Tech/> 
+            <Tech/>
+            <Tech/>
+            <Tech/>
+            <Tech/>
+        </div>
+    </div>
+</template>
