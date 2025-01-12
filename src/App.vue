06960e38e196995b2e133b1ce07e58625a37a046
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-01-13 01:00:39 +0800

    update App.vue
    layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import Header from "./components/Header.vue";
+import Header from "./components/Header.vue"
 import AboutMe from "./views/AboutMe.vue"
 </script>
 
 <template>
-  <div class="h-dvh flex justify-center bg-black">
-    <div class="w-5/12 pt-10">
+  <div class="min-h-screen flex justify-center bg-black pb-36">
+    <div class="w-4/12 pt-10">
         <Header class="pb-16"/>
         <AboutMe/>
     </div>  
