0592c02c43882906717c87b29d4644c1c0d8bb79
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-01-13 00:59:53 +0800

    create RecentProject component

---
+++ src/components/RecentProject.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="flex flex-col border border-[#575757] p-4 rounded-md">
+        <img src="../../public/tiktiktoe.png" class="w-fit">
+        <p class="font-sans font-bold text-lg text-[#FAFAFA] tracking-wide">Title here</p>
+        <p class="font-sans font-thin text-base text-[#666161] tracking-wide">Description here</p>
+    </div>
+</template>
