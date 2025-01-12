14dc846d079e6a45d59c157cbe6280d547b4aacd
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-01-13 01:00:13 +0800

    update Tech component

--- src/components/Tech.vue
+++ src/components/Tech.vue
@@ -3,8 +3,8 @@
 </script>
 
 <template>
-    <div class="w-fit flex flex-col justify-center px-16 py-2 gap-2 items-center border-[#575757] border rounded-md">
-        <img src="../../public/scala.png" class="size-10">     
+    <div class="flex flex-col justify-center px-16 py-2 gap-2 items-center border-[#575757] border rounded-md">
+        <img src="../../public/scala.png">
         <p class="font-sans font-thin text-sm tracking-wide text-[#666161]">Scala</p>
     </div>
 </template>
