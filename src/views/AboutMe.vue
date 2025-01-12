dad8dcd8c3ea962c546ccc3d3ed2fb387c645257
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-01-13 01:00:23 +0800

    update AboutMe component

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import Header from "../components/Header.vue";
-import Tech from "../components/Tech.vue";
+import Header from "../components/Header.vue"
+import Tech from "../components/Tech.vue"
+import RecentProject from "../components/RecentProject.vue"
 </script>
 
 <template>
@@ -9,7 +10,6 @@ import Tech from "../components/Tech.vue";
         <div class="pt-2 flex">
             <div class="relative">
                 <img src="../../public/profile.jpg" class="size-24 rounded-full border-4 border-[#575757]">
-                <!-- Spinner-like border effect -->
                 <div class="absolute inset-0 rounded-full border-4 border-transparent hover:border-t-[#4CAF50] hover:border-l-[#4CAF50] hover:border-r-[#4CAF50]  transition-all duration-1000 ease-in-out hover:rotate-[420deg]"></div>
             </div>
             <div class="flex flex-col pl-6 pt-2 gap-2">
@@ -17,9 +17,13 @@ import Tech from "../components/Tech.vue";
                     <p class="font-sans font-bold text-2xl text-[#FAFAFA] tracking-wider">Hey, I'm Kent</p>
                     <p class="text-2xl hover:animate-wave">👋</p>
                 </div>
-                <div class="flex items-center gap-2">
+                <!-- <div class="flex items-center gap-2">
                     <p class="text-xs">🟢</p>
                     <p class="font-sans font-thin text-base text-[#666161] tracking-wide">Available for work</p>
+                </div> -->
+                <div class="flex items-center gap-2">
+                    <p class="text-xs">📍</p>
+                    <p class="font-sans font-thin text-base text-[#666161] tracking-wide">Cebu City, Philippines</p>
                 </div>
             </div>
         </div>
@@ -33,6 +37,17 @@ import Tech from "../components/Tech.vue";
             <Tech/>
             <Tech/>
             <Tech/>
+        </div>
+        <div class="flex flex-col gap-2 pt-20">
+            <p class="font-sans font-thin text-md text-[#666161] tracking-wide">Recent Projects</p>
+            <div class="flex gap-3 justify-between">
+                <RecentProject/>
+                <RecentProject/>
+            </div>
+            <div class="flex items-center gap-2">
+                <p class="font-sans font-bold text-md text-[#FAFAFA] tracking-wide">More projects</p>
+                <img src="../../public/right-arrow.svg" class="size-4">
+            </div>
         </div>
     </div>
 </template>
