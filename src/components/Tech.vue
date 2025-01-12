776a7276b5feee7e5469d745e0f5985b8e535f47
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-01-12 18:48:21 +0800

    create Tech component

---
+++ src/components/Tech.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="w-fit flex flex-col justify-center px-16 py-2 gap-2 items-center border-[#575757] border rounded-md">
+        <img src="../../public/scala.png" class="size-10">     
+        <p class="font-sans font-thin text-sm tracking-wide text-[#666161]">Scala</p>
+    </div>
+</template>
