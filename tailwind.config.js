6fdcf6f8531378a9ffd62dc6b8686b0567445847
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-01-12 18:47:35 +0800

    update tailwind.config
    added animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wave: {
+          '0%': { transform: 'rotate(0deg)' },
+          '15%': { transform: 'rotate(14deg)' },
+          '30%': { transform: 'rotate(-8deg)' },
+          '40%': { transform: 'rotate(14deg)' },
+          '50%': { transform: 'rotate(-4deg)' },
+          '60%': { transform: 'rotate(10deg)' },
+          '70%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(0deg)' },
+        },
+      },
+      animation: {
+        wave: 'wave 1s infinite',
+      },
+      sans: ['Inconsolata', 'sans-serif'], // Replace default sans-serif
+      mono: ['Inconsolata', 'monospace'], // Use as mono if needed
+    },
   },
   plugins: [],
 }
