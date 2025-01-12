d61edd81b844cf88799820d75e07f25500f39c8e
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-01-12 18:47:53 +0800

    update App.vue
    layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-
+import Header from "./components/Header.vue";
+import AboutMe from "./views/AboutMe.vue"
 </script>
 
 <template>
-  
+  <div class="h-dvh flex justify-center bg-black">
+    <div class="w-5/12 pt-10">
+        <Header class="pb-16"/>
+        <AboutMe/>
+    </div>  
+  </div>
 </template>
 
 <style scoped>
