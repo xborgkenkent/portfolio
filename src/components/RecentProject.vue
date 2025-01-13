e2e2edc9fa52c98f604629155846f4503995c460
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-01-13 23:43:10 +0800

    update RecentProject component

--- src/components/RecentProject.vue
+++ src/components/RecentProject.vue
@@ -11,7 +11,7 @@ const props = defineProps({
 
 <template>
     <div class="w-fit flex flex-col border border-[#575757] p-4 rounded-md hover:bg-[#262626]">
-        <img :src="`../../public/${image}`" class="w-fit">
+        <img :src="`/${image}`" class="w-fit">
         <p class="font-sans font-bold text-lg text-[#FAFAFA] tracking-wide">{{ title }}</p>
         <p class="font-sans font-thin text-base text-[#666161] tracking-wide">{{ description }}</p>
     </div>
