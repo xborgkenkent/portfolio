f0d306d2024b2407b7911583eccb4f8ea1113b78
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-01-14 07:58:38 +0800

    resize component

--- src/components/Tech.vue
+++ src/components/Tech.vue
@@ -14,17 +14,17 @@ const iconSrc = computed(() =>
 </script>
 
 <template>
+  <!-- <img :src="iconSrc"/> -->
   <div
-    class="flex flex-col justify-center px-16 py-2 gap-2 items-center border-[#575757] border rounded-md hover:bg-[#262626]"
+    class="flex flex-col justify-center gap-2 pt-2 items-center border-[#575757] border rounded-md hover:bg-[#262626]"
     @mouseenter="isHovered = true"
     @mouseleave="isHovered = false"
   >
     <img
       :src="iconSrc"
       alt="icon"
-      class="transition-transform duration-300 ease-out"
+      class="transition-transform duration-300 ease-out size-8"
       :class="{ '-translate-y-1 scale-100': isHovered }"
-
     />
     <p class="font-sans font-thin text-sm tracking-wide text-[#666161]">{{ name }}</p>
   </div>
