1bc68f861ec69576a85980c8268bc94a924f47d9
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-01-16 22:54:34 +0800

    added modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="fixed inset-0 bg-black/50 flex z-50">
+        <div class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#1A1A1A] rounded-lg w-fit mx-4 z-[100]">
+        <slot></slot>
+        </div>
+    </div>
+</template>
