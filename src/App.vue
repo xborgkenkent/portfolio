1cbe242ee197579f4152db7ab3d6bc9dfbf10ac3
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-01-16 16:55:00 +0800

    maintenance

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,11 @@ import AboutMe from "./views/AboutMe.vue"
 
 <template>
   <div class="min-h-screen flex justify-center bg-[#0a0a0a] pb-36">
-    <div class="w-4/12 pt-10">
+    <p class="font-sans font-bold text-2xl text-[#FAFAFA] tracking-wider">UNDER MAINTENANCE</p>
+    <!-- <div class="w-4/12 pt-10">
         <Header class="pb-16"/>
         <AboutMe/>
-    </div>  
+    </div>   -->
   </div>
 </template>
 
