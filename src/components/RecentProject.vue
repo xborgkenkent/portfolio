a56b8148f7ad77d2c1cf0ccc45ab4b45bfdad2bb
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-01-16 15:31:35 +0800

    update font weight

--- src/components/RecentProject.vue
+++ src/components/RecentProject.vue
@@ -12,7 +12,7 @@ const props = defineProps({
 <template>
     <div class="w-fit flex flex-col border border-[#575757] p-4 rounded-md hover:bg-[#262626]">
         <img :src="`/${image}`" class="w-fit">
-        <p class="font-sans font-bold text-lg text-[#FAFAFA] tracking-wide">{{ title }}</p>
-        <p class="font-sans font-thin text-base text-[#666161] tracking-wide">{{ description }}</p>
+        <p class="font-sans font-bold text-regular text-[#FAFAFA] tracking-wide">{{ title }}</p>
+        <p class="font-sans font-thin text-thin text-[#666161]">{{ description }}</p>
     </div>
 </template>
