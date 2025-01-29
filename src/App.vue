bcb6b4cb9544e7cc6006d1f1e72b00e7ff211ded
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-01-29 16:26:40 +0800

    apply responsiveness

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import AboutMe from "./views/AboutMe.vue"
 
 <template>
   <div class="min-h-screen flex justify-center bg-[#0a0a0a] pb-36">
-    <div class="w-4/12 pt-10">
+    <div class="lg:w-6/12 lg:pt-10 w-full p-5 md:w-8/12 md:pt-10">
       <Header class="pb-16"/>
       <AboutMe/>
     </div>
