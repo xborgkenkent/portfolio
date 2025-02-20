ad3ae94e8d770f6e7f02a12af4d3212b41daae7a
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-20 19:24:46 +0800

    update app.vue

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,21 @@
 import Header from "./components/Header.vue"
 import AboutMe from "./views/AboutMe.vue"
 import { useHead } from '@vueuse/head'
-import Clarity from '@microsoft/clarity';
 
-
-// useHead({
-//   script: [
-//     {
-//       innerHTML: `
-//         (function(c,l,a,r,i,t,y){
-//           c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-//           t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-//           y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-//         })(window, document, "clarity", "script", "qcr6f8k0n4");
-//       `,
-//       type: 'text/javascript'
-//     }
-//   ]
-// })
-
-const projectId = "qcr6f8k0n4"
-
-Clarity.init(projectId);
+useHead({
+  script: [
+    {
+      innerHTML: `
+        (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "qcr6f8k0n4");
+      `,
+      type: 'text/javascript'
+    }
+  ]
+})
 
 
 </script>
